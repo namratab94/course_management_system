09384ae67906b8dfee059a2b9a77a1b2a673057c
Namrata Bilurkar <namratab94@gmail.com>
2017-11-26 16:21:51 -0500

    Wrong python version commit

--- project/README.md
+++
@@ -1,2 +0,0 @@
-# course_management_system
-Team project for the course CS 5200 taught by Prof. Nate Derbinsky during the term of Fall 2017.
--- project/cms/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/namratabilurkar/Projects/course_management_system/cms/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
