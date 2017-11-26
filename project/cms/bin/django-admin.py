5c943cdbc2d11f9e8c8bd8f8fcf9b5b4b06076e2
Namrata Bilurkar <namratab94@gmail.com>
2017-11-26 15:56:06 -0500

    Django project code

---
+++ project/cms/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/namratabilurkar/Projects/course_management_system/cms/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
