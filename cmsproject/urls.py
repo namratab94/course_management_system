e58836559d1f2b8662105980d13842802a142ee7
alex <amh>
2017-12-07 20:21:54 -0500

    brainstorming of urls done

--- cmsproject/urls.py
+++ cmsproject/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'^dashboard/', include('dashboard.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
