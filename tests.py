a128d69b96d3f85682b39b3b18d9ae1d128d9797
Namrata Bilurkar <namratab94@gmail.com>
2017-12-11 15:28:07 -0500

    Updating tests.py
    Adding details about what should be present in this file and why is it
    used.

--- tests.py
+++ tests.py
@@ -1,7 +1,13 @@
 import sys
-# if __main__():
-# a = int(sys.argv[1])
-# b = int(sys.argv[2])
+
+'''
+This file will contain the tests necessary to perform continuous integration
+of the system during development and is used by Travis-ci to perform checks 
+during each build.
+'''
+
+# Dummy test to setup Travis CI.
+
 a = 2
 b = 3
 assert a+b == 5
